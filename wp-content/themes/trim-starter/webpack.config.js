6d3cf2818bea7be36c6af81b7c63fb61024628be
Dominique Miller <dominique.p.miller@gmail.com>
2017-06-23 22:38:58 -0400

    Remove browser-sync due to incompatibility with extract-write-plugin and webpack 2+ Remove all other dep's in package.json.

--- wp-content/themes/trim-starter/webpack.config.js
+++ wp-content/themes/trim-starter/webpack.config.js
@@ -2,9 +2,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
 const webpack = require('webpack');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-
-const extractCSS = new ExtractTextPlugin({ filename: 'style.css'});
+const extractCSS = new ExtractTextPlugin({ filename: '../css/style.css'});
 
 module.exports = {
   entry: [ 'webpack-hot-middleware/client',
@@ -12,11 +10,10 @@ module.exports = {
            './assets/sass/style.scss'],
   output: {
     filename: 'bundle.min.js',
-    publicPath: '/wp-content/themes/trim-starter/assets/js/',
     path: __dirname + '/assets/js/'
   },
   resolve: {
-    extensions: ['.ts', '.js', '.scss']
+    extensions: ['.ts', '.js']
   },
   module: {
     rules: [
@@ -29,17 +26,9 @@ module.exports = {
         use: 'awesome-typescript-loader'
       },
       { 
-        //test: /\.(sass|scss)$/,
-        //use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
-      },
-      {
-        test: /\.scss$/, // files ending with .scss
-        use: ['css-hot-loader'].concat(extractCSS.extract({
-           fallback: [ 'style-loader' ],
-           use: ['css-loader', 'sass-loader'],
-           publicPath: '/wp-content/themes/trim-starter/assets/css/'
-        })),
-      },
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }   
     ]
   },
   plugins: [
