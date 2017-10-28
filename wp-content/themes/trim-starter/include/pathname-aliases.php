4696034ee8e0c58dcb1ddaba83c61b021bb77997
migueldlozano <miguel.d.lozano@gmail.com>
2017-10-28 17:25:40 -0400

    remove font assets and rename asset dirs to images, fonts

--- wp-content/themes/trim-starter/include/pathname-aliases.php
+++ wp-content/themes/trim-starter/include/pathname-aliases.php
@@ -1,5 +1,5 @@
 <?php
 
 function images_path($file_name) {
-  echo '/wp-content/themes/super-shield/assets/images/' . $file_name;
+  echo '/wp-content/themes/trim-starter/assets/images/' . $file_name;
 }
