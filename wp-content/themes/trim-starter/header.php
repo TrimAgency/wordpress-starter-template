7faa772b7349453e8395096a59c8bcbb5f72f2b6
migueldlozano <miguel.d.lozano@gmail.com>
2018-03-26 10:20:40 -0400

    style navbar and add link hover effects

--- wp-content/themes/trim-starter/header.php
+++ wp-content/themes/trim-starter/header.php
@@ -19,13 +19,13 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<style type="text/css">
-		<?php if ( 'no' === get_theme_mod( 'some-like-it-neat_post_format_support' ) ): ?>
+	<!-- <style type="text/css">
+		<?php // if ( 'no' === get_theme_mod( 'some-like-it-neat_post_format_support' ) ): ?>
 			h1.entry-title:before {
 				display: none;
 			}
-		<?php endif; ?>
-	</style>
+		<?php // endif; ?>
+	</style> -->
 
     <?php wp_head(); ?>
 </head>
@@ -41,21 +41,33 @@
   </div>
 
 
-<div class="hfeed site">
+	<div class="hfeed site">
 
-		<header id="masthead" class="site-header wrap" role="banner">
-			<!-- begin vertical nav bar -->
-		  <nav class="js-menu">
-				<?php
+		<header id="masthead" class="" role="banner">
+			<div class="top-panel">
+				<a href="/" id="logo-home-link" class="logo-home-link">
+					<div class="logo" style="background-image: url(<?php images_path('logo.svg'); ?>);" ></div>
+				</a>
+				
+
+				<button class="hamburger hamburger--spring" type="button"
+								aria-label="Menu" aria-controls="navigation">
+					<span class="hamburger-box">
+						<span class="hamburger-inner"></span>
+					</span>
+				</button>
+				<nav id="navigation" class="js-menu cl-effect-21">
+					<?php
 						wp_nav_menu(
 							array(
 								'theme_location' => 'primary-navigation',
-								'items_wrap' => '<ul>%3$s</ul>'
+								'items_wrap' => '<ul class="body light">%3$s</ul>'
 							)
 						);
-				?>
-		  </nav>
+					?>
+				</nav>
 
+			</div>
 		</header><!-- #masthead -->
 
 		<main class="site-main wrap" role="main">
